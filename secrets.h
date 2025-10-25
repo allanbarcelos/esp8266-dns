0b0c772c3347e89fb7ba7b4f988114b2029b4315
Allan Barcelos <allan@barcelos.dev>
2025-10-25 10:56:09 -0400

    first commit

---
+++ secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* ssid = "SSID";
+const char* password = "PASS";
+
+const char* github_api = "https://api.github.com/repos/allanbarcelos/esp8266-dns/releases/latest";
+
+#endif
